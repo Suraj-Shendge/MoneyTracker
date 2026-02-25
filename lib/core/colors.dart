3ad1db619cc5ec30827f1dd4e8b00f314de87a29
Suraj-Shendge <124484652+Suraj-Shendge@users.noreply.github.com>
2026-02-25 15:15:48 +0530

    Update colors.dart

--- lib/core/colors.dart
+++ lib/core/colors.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class AppColors {
+  static const background = Color(0xFF0E0F13);
+  static const card = Color(0xFF171A20);
+
+  static const emerald = Color(0xFF00D09C);
+  static const deepEmerald = Color(0xFF009E7A);
+
+  static const gold = Color(0xFFF5C542);
+  static const darkGold = Color(0xFFC99700);
+
+  static const textPrimary = Colors.white;
+  static const textSecondary = Colors.white70;
+}
